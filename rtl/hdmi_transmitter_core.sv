9226c0c23039b012154d3f988a93dce6ed1f227b
shaunsteele <shaun.steele.1020@gmail.com>
2025-03-14 00:56:30 -0700

    renamed module

--- rtl/hdmi_transmitter_core.sv
+++ rtl/hdmi_transmitter_core.sv
@@ -1,18 +1,17 @@
-// hdmi_transmitter.sv
+// hdmi_transmitter_core.sv
 
-module hdmi_transmitter #(
-    parameter string RESOLUTION = "VGA",
-    parameter string COLOR_DEPTH = 24
+module hdmi_transmitter_core #(
+    parameter string RESOLUTION = "VGA"
 )(
     input logic                         clk,
     input logic                         rstn,
 
-    input logic                         i_rgb_valid,
-    input logic     [COLOR_DEPTH-1:0]   i_rgb_pixel,
+    // input logic                         i_rgb_valid,
+    // input logic     [COLOR_DEPTH-1:0]   i_rgb_pixel,
 
     // serializer
     input logic                         ddr_clk,
-    output logic                        o_tmds[3],
+    output logic    [2:0]               o_tmds,
     output logic                        o_tmds_c
 );
 
@@ -20,7 +19,7 @@ generate
     // VGA
     // 640x480 active
     // 800x600 total
-    if (RESOLUTION = "VGA") begin
+    // if (RESOLUTION == "VGA") begin
         localparam int HActivePixels = 640;
         localparam int HFrontPorch = 16;
         localparam int HSyncWidth = 96;
@@ -30,22 +29,37 @@ generate
         localparam int VFrontPorch = 10;
         localparam int VSyncWidth = 2;
         localparam int VBackPorch = 33;
-    end
+    // end
 endgenerate
 
 localparam int HTotal = HActivePixels + HFrontPorch + HSyncWidth + HBackPorch;
 localparam int VTotal = VActivePixels + VFrontPorch + VSyncWidth + VBackPorch;
 
-
-// test_pattern
+logic [$clog2(HTotal)-1:0]    hcount;
+logic [$clog2(VTotal)-1:0]    vcount;
+logic [7:0] buffer_blu;
+logic [7:0] buffer_grn;
+logic [7:0] buffer_red;
+test_pattern_generator #(
+    .HMAX   (HTotal),
+    .VMAX   (VTotal),
+    .HA (HActivePixels),
+    .VA (VActivePixels)
+) u_TP (
+    .clk        (clk),
+    .rstn       (rstn),
+    .i_hcount   (hcount),
+    .i_vcount   (vcount),
+    .o_blu      (buffer_blu),
+    .o_grn      (buffer_grn),
+    .o_red      (buffer_red)
+);
 
 
 logic           pixel_inc;
-logic [11:0]    hcount;
-logic [11:0]    vcount;
 pixel_counter #(
     .HMAX   (HTotal),
-    .VMAX   (Vtotal)
+    .VMAX   (VTotal)
 ) u_PC (
     .clk            (clk),
     .rstn           (rstn),
@@ -87,7 +101,7 @@ tmds_encoder u_BLU (
     .clk            (clk),
     .i_data_en      (tmds_data_en),
     .i_data         (buffer_blu),
-    .i_ctrl         ({hsync, vsync}),
+    .i_ctrl         ({tmds_hsync, tmds_vsync}),
     .o_q            (tmds_d[0])
 );
 tmds_encoder u_GRN (
@@ -108,9 +122,17 @@ assign tmds_d[3] = 10'b0000011111;
 
 logic [1:0] cascade[4];
 
+logic [3:0] tmds_channels;
+assign {o_tmds_c, o_tmds[2], o_tmds[1], o_tmds[0]} = tmds_channels[3:0];
+
+logic serdes_reset = 1;
+always_ff @(posedge clk) begin
+    serdes_reset <= 0;
+end
+
 genvar i;
 generate
-    for (int i=0; i<4; i++) begin: g_tmds_serdes
+    for (i=0; i<4; i++) begin: g_tmds_serdes
         OSERDESE2 #(
             .DATA_RATE_OQ("DDR"),
             .DATA_RATE_TQ("SDR"),
@@ -119,8 +141,8 @@ generate
             .TRISTATE_WIDTH(1),
             .TBYTE_CTL("FALSE"),
             .TBYTE_SRC("FALSE")
-        ) u_OSERDES0[3:0] (
-            .OQ(tmds[i]),
+        ) u_OSERDES0 (
+            .OQ(tmds_channels[i]),
             .OFB(),
             .TQ(),
             .TFB(),
@@ -140,7 +162,7 @@ generate
             .TCE(1'b0),
             .OCE(1'b1),
             .TBYTEIN(1'b0),
-            .RST(),
+            .RST(serdes_reset),
             .SHIFTIN1(cascade[i][0]),
             .SHIFTIN2(cascade[i][1]),
             .T1(1'b0),
@@ -164,8 +186,8 @@ generate
             .SHIFTOUT1(cascade[i][0]),
             .SHIFTOUT2(cascade[i][1]),
             .TBYTEOUT(),
-            .CLK(clk_pixel_x5),
-            .CLKDIV(clk_pixel),
+            .CLK(ddr_clk),
+            .CLKDIV(clk),
             .D1(1'b0),
             .D2(1'b0),
             .D3(tmds_d[i][8]),
@@ -177,7 +199,7 @@ generate
             .TCE(1'b0),
             .OCE(1'b1),
             .TBYTEIN(1'b0),
-            .RST(reset || internal_reset),
+            .RST(serdes_reset),
             .SHIFTIN1(1'b0),
             .SHIFTIN2(1'b0),
             .T1(1'b0),
